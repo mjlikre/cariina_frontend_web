366e7c4e0b2bbe9b793053f4d105966f36300d70
Michael's <mjlikre@gmail.com>
2021-04-02 12:34:41 -0600

    done

--- src/Components/PageHeader/index.js
+++ src/Components/PageHeader/index.js
@@ -6,8 +6,8 @@ import { signout } from "../../actions";
 const PageHeader = ({history, signout, children}) => {
   return (
     <>
-      <Navbar className="navBar-color" expand="lg" variant="light">
-        <Navbar.Brand href="#home">Cariina Forms</Navbar.Brand>
+      <Navbar className="navBar-color" expand="lg" variant="dark" style = {{padding: "10px 100px"}}>
+        <Navbar.Brand href="#home"><img src = "https://assets.website-files.com/5f9950c2348f39689e28b93d/5f9956808ea7480c33768d1c_logo.svg"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
